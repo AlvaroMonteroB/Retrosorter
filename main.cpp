5f28731dc41c808bb9428a0dcb2528a285638cad
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-04-24 22:19:40 -0600

    trying a new dataset

--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
 #include"lib.h"
-
 using std::cout;
 
 void main(){
     vector<File_umbral>file_names;
-    //vector<string>names=testing_path("../test_data");    
+    //vector<string>names=testing_path("../Camera_data);    
     /*for (auto &pair:names)
     {
         File_umbral test={pair,0};
         file_names.push_back(test);
-    }*/
-    string path;
-    cout<<"Path de tu archivo\n";
-    std::cin>>path;//../test_data/omero.png
-    File_umbral test={path,0};
-    file_names.push_back(test);
-    
-    
-    
-    file_names=neural_application(file_names,"../weight_data.txt");
+}*/
 
     
 //Percept neuron1("../weight_data.txt");
