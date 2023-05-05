f9766556dcb6abc1ea4bcbe226d144e460ddd5c9
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-04 22:11:25 -0600

    comment windows

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include"lib.h"
-#include"window.h"
+//#include"window.h"
 using std::cout;
 //g++ -fopenmp main.cpp -o a
 void training();
@@ -8,7 +8,7 @@ int main(){
     Image imagen("h");
     imagen.im_read("Camera_data/train_data_camara1.bmp",false);///true = color   false = b & n 
     Image edg=imagen.canny(100,200);
-    show_image_window(edg.pixel_data(),edg.Width(),edg.Height(),edg.Channels());
+    //show_image_window(edg.pixel_data(),edg.Width(),edg.Height(),edg.Channels());
 return 0;   
 }
 
