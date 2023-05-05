bc8d5df04e7fd09a7c6039f2a2d636b538a82465
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-04 18:00:12 -0600

    main

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@ using std::cout;
 //g++ -fopenmp main.cpp -o a
 void training();
 
-void main(){
+int main(){
 Image img("name");
 img.im_read("Camera_data/train_data/camara1.bmp");
 
-    
+return 0   
 }
 
 
