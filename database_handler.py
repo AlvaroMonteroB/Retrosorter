fdd76052c57d77b391133c6b32f3a9d9bce07663
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-25 14:12:48 -0600

    get cell by name, pos and row

--- database_handler.py
+++ database_handler.py
@@ -29,16 +29,35 @@ class database:
             else:
                 types.append(3)
         self.campos=types
-
+    def get_cell(self, iterador, posicion:int):#Hacer un select directo a una celda, iterador=y, posicion=x
+        if iterador<self.pattern and iterador>=0 and posicion<self.campos and posicion>=0 and self.type_list!=None:
+            return switch_type[self.type_list[posicion]](self.data[iterador][posicion])
+        else:
+            return None
 
     def get_row(self,iterador):#Devuelve el renglon pero con los cast adecuados
         if iterador<self.pattern and iterador>=0 and self.type_list!=None:
-            aux= self.archivo[iterador]
+            aux= self.data[iterador]
             row=list()
-            for i in range(self.pattern):
-                row.append(switch_type[self.type_list[posicion]](aux[i]))                
+            for i in range(self.pattern):#switchtype== tipo de dato ,self type==int, aux==dato a castear
+                row.append(switch_type[self.type_list[i]](aux[i]))#ejem switchtpe[self.typelist[i]]=1 ||int(aux[i])
         else:
             return None
+    
+    def get_row_by_name(self,name, x): #El iterador es para el eje x ahora
+        if iterador<self.campos:
+            for row,i in self.data,range(len(self.data)):
+                if name==self.get_cell(i, x):#Si el nombre que buscamos coincide en la columna crrecta
+                    output=list()
+                    for j in range(self.pattern):
+                        output.appnd(switch_type[self.type_list[j]](row[j]))
+                        return output
+                else:
+                    continue
+
+        if len(output)==0:
+            return None
+                
 
 
 
