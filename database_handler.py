230772ca8ba782d26c1cbff6f195131ee5be1f31
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-26 14:02:10 -0600

    adjusted

--- database_handler.py
+++ database_handler.py
@@ -10,20 +10,20 @@ class database:
     def __init__(self,path):
         with open(path,'r') as f:
             self.data=list(csv.reader(f))
-        self.campos=len((self.archivo[0]))#Numero de columnas
-        self.pattern=len(list(self.archivo))#numero de filas
+        self.campos=len((self.data[0]))#Numero de columnas
+        self.pattern=len(list(self.data))#numero de filas
         self.auto_set()
     
     
     def auto_set(self):
         types=list()
-        aux=self.archivo[0]
-        for column,i in aux,range(len(aux)):
+        aux=self.data[0]
+        for i,column in enumerate(aux):
             if '.' in column:
                 types.append(2)
             elif column.isdigit():
                 count=0
-                for col in self.archivo:#Vamos a determinar si es bool
+                for col in self.data:#Vamos a determinar si es bool
                     if col[i]!=0 and  col[i]!=1:
                         count=+1
                         if count>0:
@@ -34,7 +34,7 @@ class database:
                     types.append(1)
             else:
                 types.append(3)
-        self.campos=types
+        self.type_list=types
     def get_cell(self, iterador, posicion:int):#Hacer un select directo a una celda, iterador=y, posicion=x
         if iterador<self.pattern and iterador>=0 and posicion<self.campos and posicion>=0 and self.type_list!=None:
             return switch_type[self.type_list[posicion]](self.data[iterador][posicion])
@@ -51,12 +51,12 @@ class database:
             return None
     
     def get_row_by_name(self,name, x): #El iterador es para el eje x ahora
-        if iterador<self.campos:
-            for row,i in self.data,range(len(self.data)):
+        if x<self.campos:
+            for i,row in enumerate(self.data):
                 if name==self.get_cell(i, x):#Si el nombre que buscamos coincide en la columna crrecta
                     output=list()
                     for j in range(self.pattern):
-                        output.appnd(switch_type[self.type_list[j]](row[j]))
+                        output.append(switch_type[self.type_list[j]](row[j]))
                         return output
                 else:
                     continue
